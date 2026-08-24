f39205a37cbcfd0eeda9dfce22bb30389091881f
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2026-08-24 13:48:44 +0200

    Failing test

--- src/test/java/GoedeDoelTest.java
+++ src/test/java/GoedeDoelTest.java
@@ -16,4 +16,9 @@ class GoedeDoelTest {
     void getNaam(){
         assertThat(doel.getNaam()).isEqualTo(NAAM);
     }
+
+    @Test
+    void eenNieuwDoelHeeftNogGeenOpbrengst(){
+        assertThat(doel.getOpbrengst()).isZero();
+    }
 }
